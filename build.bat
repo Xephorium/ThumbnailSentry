06a1c3ce5904dd71a8cec43a19e9b9aad7257c79
unknown <christopher.a.cruzen@gmail.com>
2022-03-14 20:35:17 -0500

    Update Build Script

--- build.bat
+++ build.bat
@@ -2,13 +2,13 @@
 @ECHO OFF
 
 :: Remove Existing Archive
-del ThumbnailSentry.xpi >NUL
+del ThumbnailSentry.xpi >nul 2>&1
 
 :: Zip Extension Files w/ Deflate Compression Method
-7z a -tzip Archive resources manifest.json ThumbnailSentry.js > NUL
+7z a -tzip Archive resources manifest.json ThumbnailSentry.js >nul
 
 :: Rename Archive
-rename Archive.zip ThumbnailSentry.xpi >NUL
+rename Archive.zip ThumbnailSentry.xpi >nul
 
 :: Print Message
 ECHO Extension packaged.
